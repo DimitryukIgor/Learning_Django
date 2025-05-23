b5def38f8079ab9f50527380b83d283f02d68364
DimitryukIgor <i.d.dimitryuk@yandex.ru>
2025-05-23 12:32:50 +0500

    Add author ForeignKey to Post model

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 class Post(models.Model):
 
@@ -9,6 +11,9 @@ class Post(models.Model):
 
     title = models.CharField(max_length=250)
     slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               related_name='blog_posts')
     body = models.TextField()
     publish = models.DateField(default=timezone.now)
     created = models.DateField(auto_now_add=True)
@@ -19,7 +24,7 @@ class Post(models.Model):
 
     class Meta:
         ordering = ['-publish']
-        index = [
+        indexes = [
             models.Index(fields=['-publish'])
         ]
     def __str__(self):
